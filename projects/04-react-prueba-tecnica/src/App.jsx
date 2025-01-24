5614b60b4b5661b32d548c490f3c69c5c05342e6
yealuis <luisjavierperez1999@gmail.com>
2025-01-23 20:56:48 -0400

    tercera clase parte 2

---
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,40 @@
+import { React, useEffect, useState } from 'react'
+import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
+export default function App() {
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const {fact} = data
+        setFact(fact)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (!fact) return
+    const threeFirstWords =fact.split(' ', 3).join(' ')
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { _id } = response
+        const url = `/cat/${_id}/says/${threeFirstWords}`
+        setImageUrl(url)
+      })
+  }, [fact])
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`}/>}
+    </main>
+  )
+}
